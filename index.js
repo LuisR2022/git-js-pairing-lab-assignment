3d98bde86fa8ab23aa4b1a1f2a237d95b206fda2
koumba-keita <koumba.keita@aol.com>
2022-09-29 18:54:54 +0000

    Question 6 completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Code your solutions in this file
+//1
 function fiveToOneHundread(){
     for(let i = 5; i <= 100; i ++){
         console.log(i);
@@ -6,13 +6,13 @@ function fiveToOneHundread(){
 }
 console.log(fiveToOneHundread())
 
-
+//2
 function multiplesOfThree(){
     for(let i = 3; i <= 100; i += 3){
         console.log(i);
     }
 }
-
+//3
 function multiplesOfThreeOrFive(){
     for(let i = 0; i <= 100; i ++){
         if( i % 3 == 0 || i % 5 == 0){
@@ -21,15 +21,25 @@ function multiplesOfThreeOrFive(){
     }
 }
 console.log(multiplesOfThreeOrFive())
-
+//4
 function untilNum(int){
     for(let i = 0; i <= int; i++){
         console.log(i)
     }
 }
 untilNum(4)
-
+//5
 function multiply(one, two){
     return one * two;
 }
 console.log(multiply(2,5))
+//6
+function add(one, two){
+    if(one == two){
+        let add2 = one + two;
+        return add2 * 3
+    }else{
+        return one + two
+    }
+}
+console.log(add(10,5));
