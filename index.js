029d460ab478cb10fe7d088f8e8e151c5e95e032
EC2 Default User <ec2-user@ip-172-31-66-159.ec2.internal>
2022-09-29 18:30:10 +0000

    soleved question 3

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ function multiplesOfThree(){
         console.log(i);
     }
 }
+/*
 multiplesOfThree();
+/*
+function multiplesOfThreeOrFive(){
+    for(let i = 3; i <= 100; i += 3){
+        console.log(i);
+    }
+}
+*/
+function multiplesOfThreeOrFive(){
+    for(let i = 0; i <= 100; i ++){
+        if( i % 3 == 0 || i % 5 == 0){
+            console.log(i)
+        }
+    }
+}
+console.log(multiplesOfThreeOrFive())
